c86ad0ba03deec642f1dda9568a2c37b820cd324
John Colvin <john.loughran.colvin@gmail.com>
2015-06-18 11:03:42 +0100

    get_pages.sh more

--- get_pages.sh
+++ get_pages.sh
@@ -2,5 +2,7 @@ set -e
 
 for pageName in dstats NetCDF-D cblas dstats PydMagic clFFT-D libcerf OpenMPI parallel_algorithm; do
     wget -O ${pageName}.md https://raw.githubusercontent.com/DlangScience/${pageName}/master/README.md
+    echo -e '---\nlayout: default\n---\n' | cat - ${pageName}.md > ${pageName}_tmp
+    mv ${pageName}_tmp ${pageName}.md
 done
 
