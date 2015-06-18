e601aae0493ad6c507a626a41b82be79e646dc63
John Colvin <john.loughran.colvin@gmail.com>
2015-06-18 12:19:52 +0100

    make dirs if not present

--- get_pages.sh
+++ get_pages.sh
@@ -1,9 +1,11 @@
 set -e
 
 for pageName in dstats NetCDF-D cblas dstats PydMagic clFFT-D libcerf OpenMPI parallel_algorithm; do
-    if [ -f repos/${pageName}/site/readme_as_index] then
+    mkdir -p ${pageName}
+    if [ -f repos/${pageName}/site/readme_as_index ]
+    then
         echo -e '---\nlayout: default\n---\n' | cat - repos/${pageName}/README.md > ${pageName}/index.md
     fi
-    cp repos/${pageName}/site ${pageName}
+    cp -r repos/${pageName}/site ${pageName}
 done
 
