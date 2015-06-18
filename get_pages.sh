375149ac693f1bcf91876ebfca754bf3b86faa19
John Colvin <john.loughran.colvin@gmail.com>
2015-06-18 12:24:36 +0100

    skip repos without site

--- get_pages.sh
+++ get_pages.sh
@@ -1,6 +1,11 @@
 set -e
 
 for pageName in dstats NetCDF-D cblas dstats PydMagic clFFT-D libcerf OpenMPI parallel_algorithm; do
+    if [ ! -d repos/${pageName}/site ]
+    then
+        echo No site directory found for repo $pageName
+        continue
+    fi
     mkdir -p ${pageName}
     if [ -f repos/${pageName}/site/readme_as_index ]
     then
